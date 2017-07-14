4517b7511d13d392a412c1fd75d410a871035d73
silentfl <silentfl@mail.ru>
2017-07-14 15:28:02 +0300

    Add enroll logic for deposit

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -2,4 +2,17 @@ class Deposit < ApplicationRecord
   monetize :amount_cents, numericality: { greater_than_or_equal_to: 0 }
 
   belongs_to :user
+
+  def enroll(direction, sum)
+    case direction
+    when 'loss'
+      self.amount -= sum
+      save!
+    when 'profit'
+      self.amount += sum
+      save!
+    else
+      raise 'Unknown direction'
+    end
+  end
 end
